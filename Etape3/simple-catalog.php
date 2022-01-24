b01fa79a09bb417e625a3646a11ec87589204781
DavidBelkacem <david.belkacem@le-campus-numerique.fr>
2022-01-24 14:39:30 +0100

    Etape 3 - Exercice 1

---
+++ Etape3/simple-catalog.php
@@ -0,0 +1,9 @@
+<?php
+$products = ["iPhone", "iPad", "iMac"];
+echo "Before sorting :" . "<br>";
+print_r($products);
+sort($products);
+echo "<br>" . "After sorting :" . "<br>";
+print_r($products);
+echo "<br>" . "$products[0]" . "<br>";
+echo $products[2];
