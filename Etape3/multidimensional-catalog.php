e76afbbcdbe1620cbd046a3072fee92d42afecca
DavidBelkacem <david.belkacem@le-campus-numerique.fr>
2022-01-25 13:19:40 +0100

    ajout multidimensional-catalog

---
+++ Etape3/multidimensional-catalog.php
@@ -0,0 +1,21 @@
+<?php 
+$products = [
+    "banana" => [
+        "name" => "banana",
+        "price" => 2,
+        "weight" => 1,
+        "discount" => null,
+        "picture_url" => "https://www.jelly-shop.fr/resize/FABF6B_13788762637760.jpg/0/1100/True/jellycat-fruit-fabuleux-banane-17cm.jpg",
+    ],
+    "pineapple" => [
+        "name" => "pineapple",
+        "price" => 2.5,
+        "weight" => 0.8,
+        "discount" => 10,
+        "picture_url" => "https://lescureviandesetprimeur.com/wp-content/uploads/2020/04/ananas-pain-de-sucre-cayenne.jpg",
+    ],   
+];
+
+echo '<pre>';
+var_dump($products);
+
