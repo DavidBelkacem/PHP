f660f4bf821e8edf34fe8d3547a46a2a5c40f343
DavidBelkacem <david.belkacem@le-campus-numerique.fr>
2022-01-28 16:58:39 +0100

    Etape 7 - 2

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+    include "./array_products.php"
+
+    foreach ($products as $fruit => $features) {
+        foreach ($features as $feature_name => $value) {
+            echo 
+        }
+    }
